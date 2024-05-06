ee1d873e6deb3ece8076c0e80fa963b49caa5a35
nadu <nadu@WINDOWS-UINETAB>
2024-05-06 03:40:54 -0700

    Add project files.

---
+++ labsheet 01 question 3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0;
+
+            Console.WriteLine("Enter a positive number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    sum = sum + i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
